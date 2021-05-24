f3066b206d65001ad9434581cfb80b4e3cdbe28b
Michel Albert <michel@albert.lu>
2021-05-24 11:17:15 +0200

    Encapsulate episode-key function into Config class

--- ordering/static/js/index.js
+++ ordering/static/js/index.js
@@ -16,6 +16,20 @@ class Config {
     };
 
     /**
+     * Convert the "data-series" and "data-episode-id" into a sinlge key for
+     * LocalStorage.
+     * 
+     * @param  {Element} element the element which contains the data attributes.
+     * @returns A string identifying the episode
+     */
+    static getEpisodeKey (element) {
+        let series = element.attributes["data-series"].value;
+        let episode = element.attributes["data-episode-id"].value;
+        let key = `${series}-${episode}`;
+        return key;
+    }
+
+    /**
      * Retrieve a value from persistent user-storage.
      * 
      * @param {String} key The name/key of the config-value to retrieve
@@ -118,20 +132,6 @@ class Config {
     };
 
     /**
-     * Convert the "data-series" and "data-episode-id" into a sinlge key for
-     * LocalStorage.
-     * 
-     * @param  {Element} element the element which contains the data attributes.
-     * @returns A string identifying the episode
-     */
-    var getLSEpisodeKey = function (element) {
-        let series = element.attributes["data-series"].value;
-        let episode = element.attributes["data-episode-id"].value;
-        let key = `${series}-${episode}`;
-        return key;
-    }
-
-    /**
      * Updates localStorage with "watched" information of an episode
      * 
      * 
@@ -139,7 +139,7 @@ class Config {
      */
     var updateWatched = function (evt) {
         let newValue = evt.target.checked;
-        let key = getLSEpisodeKey(evt.target);
+        let key = Config.getEpisodeKey(evt.target);
         let watchedEpisodes = config.get(Config.WATCHED_EPISODES, []);
         let index = watchedEpisodes.indexOf(key)
         if (newValue === true && index === -1) {
@@ -162,7 +162,7 @@ class Config {
             doHide ? watchedStateClasses.HIDDEN : watchedStateClasses.FAINT
         );
         $('.episode').map(function() {
-            let key = getLSEpisodeKey(this);
+            let key = Config.getEpisodeKey(this);
             for (const [_, value] of Object.entries(watchedStateClasses)) {
                 $(this).removeClass(value)
             }
